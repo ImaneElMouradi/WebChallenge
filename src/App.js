866476e2edf0c7f3c6b48137cf84341c4559b9bd
Imane <imaneelmouradi98@gmail.com>
2019-04-28 13:52:53 +0100

    organized components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 
 import "./App.css";
-import RepoList from "./components/RepoList";
+import Repositories from "./components/Repositories";
 
 class App extends Component {
   render() {
-    return <RepoList />;
+    return <Repositories />;
   }
 }
 
